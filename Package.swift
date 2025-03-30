6d5d69222f42f0ef7504d1dadd789b67dd7a8759
Mohamad <mo.rajbi@gmail.com>
2025-03-30 14:18:09 +0330

    Inline GRDB.swift 3.1.0 (GRDB 7.4.1, SQLCipher 4.7.0)

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "GRDB",
-            url: "https://github.com/inline-chat/GRDB.swift/releases/download/3.0.8/GRDB.xcframework.zip",
-            checksum: "02849a50c8649b64cf7d844374506313e9ccac21925284c5f65ea2ee48c89f9c"
+            url: "https://github.com/inline-chat/GRDB.swift/releases/download/3.1.0/GRDB.xcframework.zip",
+            checksum: "f6fa67a7ae52c99f96f7859e506c2186e30313c92a50dd7f7a9303af2b5a7bf3"
         ),
         .target(name: "_GRDBDummy")
     ]
